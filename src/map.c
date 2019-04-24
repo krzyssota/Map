da6055a094a88a2c8c4f7dff1e770dc9f8514a47
Krzysztof Sota <k.sota@student.uw.edu.pl>
2019-04-24 13:39:51 +0200

    First bits

---
+++ src/map.c
@@ -0,0 +1,287 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// lista miast, kazde ma liste dróg, miasto ma wskaznik na poczatek (i koniec ? ) drogi krajowej na ktorej sie znajduje
+typedef struct City City;
+typedef struct Road Road;
+typedef struct RouteSegment RouteSegment;
+typedef struct RouteList RouteList;
+typedef struct RoadList RoadList;
+
+typedef struct VectorRoutes{
+    int* IDs;
+    int filled;
+    int size;
+} RouteV;
+
+typedef struct VectorCities{
+    City** cities;
+    int filled;
+    int size;
+} CityV;
+
+typedef struct RouteSegment{
+
+    RouteSegment* nextSegment;
+    RouteSegment* prevSegment;
+    Road* road;
+    unsigned routeId;
+
+
+} RouteSegment;
+
+typedef struct RouteList{
+
+    RouteList* prevRoute;
+    RouteList* nextRoute;
+    RouteSegment* start;
+
+} RouteList;
+
+struct Road{
+
+    City* from;
+    City* to;
+
+    unsigned length;
+    int year;
+
+    Vector* routes;
+
+    /*unsigned routeId;
+    City* routeFrom;
+    City* routeTo;
+    struct Road* nextRoadInRoute;*/
+
+};
+
+typedef struct RoadList{
+
+    RoadList* prev;
+    RoadList* next;
+    Road* road;
+
+} RoadList;
+
+struct City{ // city vector? binsearch? czy to nie jest i tak taka sama zlozonosc jak musimy wczesniej wstawiac rosnaco te miasta?
+
+    char* name;
+    RoadList* firstRoad;
+    int proximity;
+
+    /*City* prevCity;
+    City* nextCity;*/
+
+
+};
+bool findCity(City* city, const char *city1, City* cityFrom);
+
+
+/*typedef struct CityList{
+
+    City* city;
+    struct CityList* next;
+
+} CityList;*/
+
+typedef struct Map{
+
+    CityV cities;
+    RouteList* firstRoute;
+
+} Map;
+
+
+/** @brief Tworzy nową strukturę.
+ * Tworzy nową, pustą strukturę niezawierającą żadnych miast, odcinków dróg ani
+ * dróg krajowych.
+ * @return Wskaźnik na utworzoną strukturę lub NULL, gdy nie udało się
+ * zaalokować pamięci.
+ */
+Map* newMap(void){
+    Map* newMap = malloc(sizeof(Map));
+    newMap->firstCity = NULL;
+}
+
+/** @brief Usuwa strukturę.
+ * Usuwa strukturę wskazywaną przez @p map.
+ * Nic nie robi, jeśli wskaźnik ten ma wartość NULL.
+ * @param[in] map        – wskaźnik na usuwaną strukturę.
+ */
+void deleteMap(Map *map){
+    deleteCities(map->firstCity);
+    deleteRoutes(map->firstRoute);
+    free(map);
+}
+
+/** @brief Dodaje do mapy odcinek drogi między dwoma różnymi miastami.
+ * Jeśli któreś z podanych miast nie istnieje, to dodaje go do mapy, a następnie
+ * dodaje do mapy odcinek drogi między tymi miastami.
+ * @param[in,out] map    – wskaźnik na strukturę przechowującą mapę dróg;
+ * @param[in] city1      – wskaźnik na napis reprezentujący nazwę miasta;
+ * @param[in] city2      – wskaźnik na napis reprezentujący nazwę miasta;
+ * @param[in] length     – długość w km odcinka drogi;
+ * @param[in] builtYear  – rok budowy odcinka drogi.
+ * @return Wartość @p true, jeśli odcinek drogi został dodany.
+ * Wartość @p false, jeśli wystąpił błąd: któryś z parametrów ma niepoprawną
+ * wartość, obie podane nazwy miast są identyczne, odcinek drogi między tymi
+ * miastami już istnieje lub nie udało się zaalokować pamięci.
+ */
+bool addRoad(Map *map, const char *city1, const char *city2, unsigned length, int builtYear){
+
+    Road* newRoad = malloc(sizeof(Road));
+    if(!newRoad) return false;
+
+    City* cityFrom;
+    if(strcmp(city1, city2) == 0){
+        free(newRoad);
+        return false;
+    }
+    if(!findCity(map, city1, cityFrom)){
+        free(newRoad);
+        return false;
+    }
+
+    City* cityTo;
+    if(!findCity(map, city2, cityTo)){
+        free(newRoad);
+        return false;
+    }
+
+    if(cityFrom == cityTo) {
+        free(newRoad);
+        return false;
+    }
+
+    newRoad->from = cityFrom;
+    newRoad->to = cityTo;
+
+    newRoad->length = length;
+    newRoad->year = builtYear;
+
+    if(!roadToList(cityFrom->firstRoad, newRoad)){
+        free(newRoad);
+        return false;
+    }
+    return true;
+}
+
+/** @brief Modyfikuje rok ostatniego remontu odcinka drogi.
+ * Dla odcinka drogi między dwoma miastami zmienia rok jego ostatniego remontu
+ * lub ustawia ten rok, jeśli odcinek nie był jeszcze remontowany.
+ * @param[in,out] map    – wskaźnik na strukturę przechowującą mapę dróg;
+ * @param[in] city1      – wskaźnik na napis reprezentujący nazwę miasta;
+ * @param[in] city2      – wskaźnik na napis reprezentujący nazwę miasta;
+ * @param[in] repairYear – rok ostatniego remontu odcinka drogi.
+ * @return Wartość @p true, jeśli modyfikacja się powiodła.
+ * Wartość @p false, jeśli wystąpił błąd: któryś z parametrów ma niepoprawną
+ * wartość, któreś z podanych miast nie istnieje, nie ma odcinka drogi między
+ * podanymi miastami, podany rok jest wcześniejszy niż zapisany dla tego odcinka
+ * drogi rok budowy lub ostatniego remontu.
+ */
+bool repairRoad(Map *map, const char *city1, const char *city2, int repairYear) {
+
+    City *cityFrom;
+    if(!findCity(map->firstCity, city1, cityFrom)){
+        return false;
+    }
+    City *cityTo;
+    if(!findCity(map->firstCity, city2, cityTo)){
+        return false;
+    }
+    Road* road;
+    if(!findRoad(cityFrom->firstRoad, cityTo, road)){
+        return false;
+    }
+    if(repairYear >= road->year){
+        road->year = repairYear;
+        return true;
+    }
+    return false;
+}
+
+/** @brief Łączy dwa różne miasta drogą krajową.
+ * Tworzy drogę krajową pomiędzy dwoma miastami i nadaje jej podany numer.
+ * Wśród istniejących odcinków dróg wyszukuje najkrótszą drogę. Jeśli jest
+ * więcej niż jeden sposób takiego wyboru, to dla każdego wariantu wyznacza
+ * wśród wybranych w nim odcinków dróg ten, który był najdawniej wybudowany lub
+ * remontowany i wybiera wariant z odcinkiem, który jest najmłodszy.
+ * @param[in,out] map    – wskaźnik na strukturę przechowującą mapę dróg;
+ * @param[in] routeId    – numer drogi krajowej;
+ * @param[in] city1      – wskaźnik na napis reprezentujący nazwę miasta;
+ * @param[in] city2      – wskaźnik na napis reprezentujący nazwę miasta.
+ * @return Wartość @p true, jeśli droga krajowa została utworzona.
+ * Wartość @p false, jeśli wystąpił błąd: któryś z parametrów ma niepoprawną
+ * wartość, istnieje już droga krajowa o podanym numerze, któreś z podanych
+ * miast nie istnieje, obie podane nazwy miast są identyczne, nie można
+ * jednoznacznie wyznaczyć drogi krajowej między podanymi miastami lub nie udało
+ * się zaalokować pamięci.
+ */
+bool newRoute(Map *map, unsigned routeId, const char *city1, const char *city2);
+
+/** @brief Wydłuża drogę krajową do podanego miasta.
+ * Dodaje do drogi krajowej nowe odcinki dróg do podanego miasta w taki sposób,
+ * aby nowy fragment drogi krajowej był najkrótszy. Jeśli jest więcej niż jeden
+ * sposób takiego wydłużenia, to dla każdego wariantu wyznacza wśród dodawanych
+ * odcinków dróg ten, który był najdawniej wybudowany lub remontowany i wybiera
+ * wariant z odcinkiem, który jest najmłodszy.
+ * @param[in,out] map    – wskaźnik na strukturę przechowującą mapę dróg;
+ * @param[in] routeId    – numer drogi krajowej;
+ * @param[in] city       – wskaźnik na napis reprezentujący nazwę miasta.
+ * @return Wartość @p true, jeśli droga krajowa została wydłużona.
+ * Wartość @p false, jeśli wystąpił błąd: któryś z parametrów ma niepoprawną
+ * nazwę, nie istnieje droga krajowa o podanym numerze, nie ma miasta o podanej
+ * nazwie, przez podane miasto już przechodzi droga krajowa o podanym numerze,
+ * podana droga krajowa kończy się w podanym mieście, nie można jednoznacznie
+ * wyznaczyć nowego fragmentu drogi krajowej lub nie udało się zaalokować
+ * pamięci.
+ */
+bool extendRoute(Map *map, unsigned routeId, const char *city);
+
+/** @brief Usuwa odcinek drogi między dwoma różnymi miastami.
+ * Usuwa odcinek drogi między dwoma miastami. Jeśli usunięcie tego odcinka drogi
+ * powoduje przerwanie ciągu jakiejś drogi krajowej, to uzupełnia ją
+ * istniejącymi odcinkami dróg w taki sposób, aby była najkrótsza. Jeśli jest
+ * więcej niż jeden sposób takiego uzupełnienia, to dla każdego wariantu
+ * wyznacza wśród dodawanych odcinków drogi ten, który był najdawniej wybudowany
+ * lub remontowany i wybiera wariant z odcinkiem, który jest najmłodszy.
+ * @param[in,out] map    – wskaźnik na strukturę przechowującą mapę dróg;
+ * @param[in] city1      – wskaźnik na napis reprezentujący nazwę miasta;
+ * @param[in] city2      – wskaźnik na napis reprezentujący nazwę miasta.
+ * @return Wartość @p true, jeśli odcinek drogi został usunięty.
+ * Wartość @p false, jeśli z powodu błędu nie można usunąć tego odcinka drogi:
+ * któryś z parametrów ma niepoprawną wartość, nie ma któregoś z podanych miast,
+ * nie istnieje droga między podanymi miastami, nie da się jednoznacznie
+ * uzupełnić przerwanego ciągu drogi krajowej lub nie udało się zaalokować
+ * pamięci.
+ */
+bool removeRoad(Map *map, const char *city1, const char *city2);
+
+/** @brief Udostępnia informacje o drodze krajowej.
+ * Zwraca wskaźnik na napis, który zawiera informacje o drodze krajowej. Alokuje
+ * pamięć na ten napis. Zwraca pusty napis, jeśli nie istnieje droga krajowa
+ * o podanym numerze. Zaalokowaną pamięć trzeba zwolnić za pomocą funkcji free.
+ * Informacje wypisywane są w formacie:
+ * numer drogi krajowej;nazwa miasta;długość odcinka drogi;rok budowy lub
+ * ostatniego remontu;nazwa miasta;długość odcinka drogi;rok budowy lub
+ * ostatniego remontu;nazwa miasta;…;nazwa miasta.
+ * Kolejność miast na liście jest taka, aby miasta @p city1 i @p city2, podane
+ * w wywołaniu funkcji @ref newRoute, które utworzyło tę drogę krajową, zostały
+ * wypisane w tej kolejności.
+ * @param[in,out] map    – wskaźnik na strukturę przechowującą mapę dróg;
+ * @param[in] routeId    – numer drogi krajowej.
+ * @return Wskaźnik na napis lub NULL, gdy nie udało się zaalokować pamięci.
+ */
+char const* getRouteDescription(Map *map, unsigned routeId);
+
+bool findCity(City* city, const char* city1, City* result) {
+
+    while(city != NULL && strcmp(city->name, city1) != 0){
+        city = city->nextCity;
+    }
+    result = city;
+
+    if(result == NULL) return false;
+    return true;
+}
